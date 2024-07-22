1657535ab251100ac6efe3359b4f5fd47ce5a1c9
zaz7 <abdullah75farid@gmail.com>
2024-07-22 21:21:56 +0300

    dehfejbf

--- javascript/contact.js
+++ javascript/contact.js
@@ -4,3 +4,9 @@ const error_name = document.querySelector(".errorn");
 const error_email = document.querySelector(".errore");
 const error_comment = document.querySelector(".errorc");
 
+const submit_buttom = document.querySelector(".submit");;
+
+
+submit_buttom.addEventListener("click",()=>{
+    
+})
