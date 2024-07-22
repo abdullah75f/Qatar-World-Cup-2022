335e714d2b69a21957df701cd34165847cf7e4e1
zaz7 <abdullah75farid@gmail.com>
2024-07-22 23:57:16 +0300

    efefef

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,12 +1,48 @@
-
+//error messages to be displayed
 const error_name = document.querySelector(".errorn");
 
 const error_email = document.querySelector(".errore");
 const error_comment = document.querySelector(".errorc");
 
-const submit_buttom = document.querySelector(".submit");;
+//the input fields
 
+const nname = document.querySelector("#fullname");
 
-submit_buttom.addEventListener("click",()=>{
+const email = document.querySelector("#email");
+const comment = document.querySelector("#comment");
+
+//submit button
+
+const submit_button = document.querySelector(".submit");
+
+console.log(submit_button);
+
+// const form = document.getElementById("myForm");
+
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault();
     
+
+   
+
+// })
+
+
+submit_button.addEventListener("click",(e)=>{
+    e.preventDefault();
+    
+
+    // if(nname === "" ){
+    //     error_name.style.display = "block";
+  // }
+
 })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const form = document.getElementById('myForm');
+
+//     form.addEventListener('submit', function (event) {
+//         // Prevent the form from submitting
+//         event.preventDefault();
+//     });
+// });
